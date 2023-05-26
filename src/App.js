ba65790bce745f17abec9877d216caa80cf23b4a
Yash0125 <116873935+Yash0125@users.noreply.github.com>
2023-05-26 19:43:05 +0530

    feat

--- src/App.js
+++ src/App.js
@@ -1,10 +1,183 @@
-import './App.css';
+import "./App.css";
+import React, { useState, useEffect, useRef } from "react";
+import randomWords from "random-words";
+
+const NUMB_OF_WORDS = 200;
+const SECONDS = 300;
 
 function App() {
+  const [words, setWords] = useState([]);
+  const [countDown, setCountDown] = useState(SECONDS);
+  const [currInput, setCurrInput] = useState("");
+  const [currWordIndex, setCurrWordIndex] = useState(0);
+  const [currCharIndex, setCurrCharIndex] = useState(-1);
+  const [currChar, setCurrChar] = useState("");
+  const [correct, setCorrect] = useState(0);
+  const [incorrect, setIncorrect] = useState(0);
+  const [status, setStatus] = useState("waiting");
+  const textInput = useRef(null);
+
+  useEffect(() => {
+    setWords(generateWords());
+  }, []);
+
+  useEffect(() => {
+    if (status === "started") {
+      textInput.current.focus();
+    }
+  }, [status]);
+
+  function generateWords() {
+    return new Array(NUMB_OF_WORDS).fill(null).map(() => randomWords());
+  }
+
+  function start() {
+    if (status === "finished") {
+      setWords(generateWords());
+      setCurrWordIndex(0);
+      setCorrect(0);
+      setIncorrect(0);
+      setCurrCharIndex(-1);
+      setCurrChar("");
+    }
+
+    if (status !== "started") {
+      setStatus("started");
+      let interval = setInterval(() => {
+        setCountDown((prevCountdown) => {
+          if (prevCountdown === 0) {
+            clearInterval(interval);
+            setStatus("finished");
+            setCurrInput("");
+            return SECONDS;
+          } else {
+            return prevCountdown - 1;
+          }
+        });
+      }, 1000);
+    }
+  }
+
+  function handleKeyDown({ keyCode, key }) {
+    // space bar
+    if (keyCode === 32) {
+      checkMatch();
+      setCurrInput("");
+      setCurrWordIndex(currWordIndex + 1);
+      setCurrCharIndex(-1);
+      // backspace
+    } else if (keyCode === 8) {
+      setCurrCharIndex(currCharIndex - 1);
+      setCurrChar("");
+    } else {
+      setCurrCharIndex(currCharIndex + 1);
+      setCurrChar(key);
+    }
+  }
+
+  function checkMatch() {
+    const wordToCompare = words[currWordIndex];
+    const doesItMatch = wordToCompare === currInput.trim();
+    if (doesItMatch) {
+      setCorrect(correct + 1);
+    } else {
+      setIncorrect(incorrect + 1);
+    }
+  }
+
+  function getCharClass(wordIdx, charIdx, char) {
+    if (
+      wordIdx === currWordIndex &&
+      charIdx === currCharIndex &&
+      currChar &&
+      status !== "finished"
+    ) {
+      if (char === currChar) {
+        return "has-background-success";
+      } else {
+        return "has-background-danger";
+      }
+    } else if (
+      wordIdx === currWordIndex &&
+      currCharIndex >= words[currWordIndex].length
+    ) {
+      return "has-background-danger";
+    } else {
+      return "";
+    }
+  }
+
   return (
-    <div>
-      
-    </div>
+    <>
+      <div className="section">
+        <div className="timer">
+          <h2>
+            {Math.floor(countDown / 60)}:
+            {(countDown % 60).toString().padStart(2, "0")}
+          </h2>
+        </div>
+      </div>
+      <div className="input-control">
+        <input
+          ref={textInput}
+          disabled={status !== "started"}
+          type="text"
+          className="input"
+          onKeyDown={handleKeyDown}
+          value={currInput}
+          onChange={(e) => setCurrInput(e.target.value)}
+        />
+      </div>
+      <div className="section">
+        <button className="start-btn" onClick={start}>
+          Start
+        </button>
+      </div>
+      {status === "started" && (
+        <div className="section">
+          <div className="card">
+            <div className="card-content">
+              <div className="content">
+                {words.map((word, i) => (
+                  <span key={i}>
+                    <span>
+                      {word.split("").map((char, idx) => (
+                        <span className={getCharClass(i, idx, char)} key={idx}>
+                          {char}
+                        </span>
+                      ))}
+                    </span>
+                    <span> </span>
+                  </span>
+                ))}
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+      {status === "finished" && (
+        <div className="section">
+          <div className="columns">
+            <div className="wpm">
+              <p className="result-text">Words per minute:</p>
+              <p className="display-result">
+                {Math.round(correct / (SECONDS / 60))}
+              </p>
+            </div>
+            <div className="accuracy">
+              <p className="result-text">Accuracy:</p>
+              {correct !== 0 ? (
+                <p className="display-result">
+                  {Math.round((correct / (correct + incorrect)) * 100)}%
+                </p>
+              ) : (
+                <p className="display-result">0%</p>
+              )}
+            </div>
+          </div>
+        </div>
+      )}
+    </>
   );
 }
 
